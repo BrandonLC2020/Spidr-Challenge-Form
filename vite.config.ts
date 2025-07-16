2e176ba95a6c7e5d1781452ed7294195d2bab2d9
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2025-07-16 08:08:55 -0400

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(() => {
-    return {
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/Spidr-Challenge-Form/', 
+})
